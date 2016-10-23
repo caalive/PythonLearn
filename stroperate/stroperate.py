dfb2e55b407bc64c1b98d800ad3e935a081e43bb
caalive <caa502@live.com>
2016-10-23 23:08:38 +0800

    list code exercise copy and deep copy print the memory address of variable

--- stroperate/stroperate.py
+++ stroperate/stroperate.py
@@ -4,6 +4,7 @@
 
 # names = []   #empty list
 
+import copy
 names = ['ZhangSan', 'LiSi', 'WangWu', 'LiuQi']
 # print(names[0])
 # print(names)
@@ -44,11 +45,54 @@ names = ['ZhangSan', 'LiSi', 'WangWu', 'LiuQi']
 
 # names.reverse()
 # names.sort()
-name2 = [1, 2, 3, 4]
+name2 = []
+
+names.append(['CAA','chen'])
+names[4][1] = 'CHEN'
+
+
+name2 = names.copy()  #shandow copy()
+
+print(names,name2)
+
+names[0] = 'JIAJIA'
 
 # print(names)
-names.extend(name2)  # extend source
+#names.extend(name2)  # extend source
 
-print(names)
 
-del name2
+#print(names,name2)
+
+#names = name2
+
+#print(names,name2)
+
+#print(names is name2)
+#del names[0]
+
+l = 5;
+k = 6;
+
+print(hex(id(l)))
+print(hex(id(k)))
+
+l = k       #单个变量之间的赋值等于 打印地址后为深拷贝原理 两个变量 指向为不同的内存地址
+
+print(hex(id(names)))  #print the memory address of names  variable
+print(hex(id(name2)))  #print the memory address of name2  variable
+
+print(names,name2)
+
+name2 = names #列表之间的赋值等于 打印地址后为引用效果 两个变量指向同一块内存地址
+
+print(hex(id(names)))
+print(hex(id(name2)))
+
+for i in names:
+    print((hex(id(i))),i)
+
+
+
+
+
+#del name2
